c044d19af5dc508e0e7f094355c38731b93dc735
alumno <alumno@Instituto.local>
2025-09-10 08:13:54 -0300

    10/9

--- script.js
+++
@@ -1,6 +0,0 @@
-fetch("https://api.nasa.gov/planetary/apod?api_key=f5KYE3JIAP1PA5YMc3gFvp0gHU2V6efU00psWP0K")
-  .then(res => res.json())
-  .then(datos => {
-    console.log(datos.title); // Muestra el título en consola
-    console.log(datos.url);   // Muestra la URL de la imagen/video
-  });
